3fd7e78681d7286b3e58da201775263eeaeedd76
net-blip <jose@wespark.io>
2026-02-04 09:41:52 -0600

    Clean up logo assignment in dashboard component
    Remove TypeScript ignore comment for logo assignment.

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -208,7 +208,6 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
         switchMap((blocks) => {
           if (this.stateService.env.MINING_DASHBOARD === true) {
             for (const block of blocks) {
-              // @ts-ignore: Need to add an extra field for the template
               block.extras.pool.logo = `/resources/mining-pools/` +
                 block.extras.pool.slug + '.svg';
             }
